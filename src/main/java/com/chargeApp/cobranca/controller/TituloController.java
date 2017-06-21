b8cfa1213b3fa273219c5cfb3e370a3aa24cb4ef
Tauan Tathiell <lukyhenson@live.com>
2017-06-21 08:50:29 -0300

    006

--- src/main/java/com/chargeApp/cobranca/controller/TituloController.java
+++ src/main/java/com/chargeApp/cobranca/controller/TituloController.java
@@ -61,6 +61,13 @@ public class TituloController {
         return mv;
     }
 
+    @RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+    public String excluir(@PathVariable Long codigo, RedirectAttributes attributes){
+        titulos.delete(codigo);
+        attributes.addFlashAttribute("mensagem", "Título excluido com sucesso!");
+        return "redirect:/titulos";
+    }
+
     @ModelAttribute("todosStatusTitulo")
     public List<StatusTitulo> todosStatusTitulo() {
         return Arrays.asList(StatusTitulo.values());
